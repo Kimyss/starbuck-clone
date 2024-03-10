91439b354679800bb074549301aaa5abb7d965be
kimyss <kimyslive7@naver.com>
2024-03-10 22:13:34 +0900

    확인
    확인용도

--- JS/youtube.js
+++ JS/youtube.js
@@ -18,11 +18,11 @@ function onYouTubeIframeAPIReady() {      //라이브러리가 이 함수의 이
   player = new YT.Player('player', {      //id가 player인 요소
     // height: '360',
     // width: '640',
-    videoId: 'KMbn59MEhAw',     //최초 재생할 유튜브 영상 ID
+    videoId: 'HwVEgnyeFMg',     //최초 재생할 유튜브 영상 ID
     playerVars:{                // 더 자세한 옵션은 플레이어 매개변수 메뉴 확인
       autoplay: true,           //자동 재생 유무
       loop: true,               //반복 재생 유무(아래 플레이리스트 옵션필수)
-      playlist:'KMbn59MEhAw'    //반복 재생할 유튜브 영상 ID 목록
+      playlist:'HwVEgnyeFMg'    //반복 재생할 유튜브 영상 ID 목록
     },
     events: {
       // 영상이 준비 되었을때 지정한 함수를 실행 onReady
